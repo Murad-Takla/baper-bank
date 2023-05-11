6c4ffe36796639420152e62109ae23e124b54147
Murad-Takla <irfanchowdhurycr7@gmail.com>
2023-05-11 22:34:17 +0600

    Baper Bank is ready after edit

--- login.js
+++ login.js
@@ -1,13 +1,21 @@
 document.getElementById('submit-btn').addEventListener('click' , function(){
-    const emailField = document.getElementById('email-login').value
-    const pass = document.getElementById('password').value
+    const emailField = document.getElementById('email-login')
+    const emailText = emailField.value
     
-    if (emailField==='irfan@gmail.com' && pass==='secret')
+    emailField.value = '';
+
+    const pass = document.getElementById('password') ; 
+    const passText = pass.value;
+    pass.value ='';
+    
+    if (emailText==='irfan@gmail.com' && passText==='secret')
     {
         window.location.href = "bank.html";
     }
     else{
         alert('Your email or password is not correct')
+        
+
     }
 })
 // console.log('THIS IS YOU MAIN PART')
